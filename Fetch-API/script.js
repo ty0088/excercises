85f7e3a4a6c8a27b3c9e2eea0bf8986f9a7537e1
ty0088 <95297807+ty0088@users.noreply.github.com>
2022-05-16 11:21:53 +0100

    Added Giphy API exercise

---
+++ Fetch-API/script.js
@@ -0,0 +1,34 @@
+function fetchGif(searchVal) {
+    fetch(`https://api.giphy.com/v1/gifs/translate?api_key=itg7Lp951dtU1p6G6ZeJ1j1iSnSJRDbG&s=${searchVal}`, {mode: 'cors'})
+    .then((response) => {
+        return response.json();
+    })
+    .then((response) => {
+        img.src = response.data.images.original.url;
+    })
+    .catch((err) => {
+        console.log(err);
+        img.src = './wrong-GIPHY.gif';
+    });  
+}
+
+const img = document.querySelector('img');
+let searchVal = document.getElementById('searchInput').value;
+const searchGIF = document.getElementById('searchBtn');
+const refreshGIF = document.getElementById('refreshBtn');
+const inputBox = document.getElementById('searchInput');
+
+searchGIF.addEventListener('click', () => {
+    if (document.getElementById('searchInput').value !== '') {
+        searchVal = document.getElementById('searchInput').value;
+        fetchGif(searchVal);
+    }
+});
+
+refreshGIF.addEventListener('click', () => {
+    fetchGif(searchVal);
+});
+
+window.onload = (()=>{
+    fetchGif(searchVal);
+});
