d8e2afeecc48d0bfa094bef7135a1c5443c0799f
ty0088 <95297807+ty0088@users.noreply.github.com>
2022-05-16 12:15:27 +0100

    Refactored using Async/Await

--- Fetch-API/script.js
+++ Fetch-API/script.js
@@ -1,15 +1,26 @@
-function fetchGif(searchVal) {
-    fetch(`https://api.giphy.com/v1/gifs/translate?api_key=itg7Lp951dtU1p6G6ZeJ1j1iSnSJRDbG&s=${searchVal}`, {mode: 'cors'})
-    .then((response) => {
-        return response.json();
-    })
-    .then((response) => {
-        img.src = response.data.images.original.url;
-    })
-    .catch((err) => {
+// function fetchGif(searchVal) {
+//     fetch(`https://api.giphy.com/v1/gifs/translate?api_key=itg7Lp951dtU1p6G6ZeJ1j1iSnSJRDbG&s=${searchVal}`, {mode: 'cors'})
+//     .then((response) => {
+//         return response.json();
+//     })
+//     .then((response) => {
+//         img.src = response.data.images.original.url;
+//     })
+//     .catch((err) => {
+//         console.log(err);
+//         img.src = './wrong-GIPHY.gif';
+//     });  
+// }
+
+async function fetchGif(searchVal) {
+    try {
+        const response = await fetch(`https://api.giphy.com/v1/gifs/translate?api_key=itg7Lp951dtU1p6G6ZeJ1j1iSnSJRDbG&s=${searchVal}`, {mode: 'cors'});
+        const gifData = await response.json();
+        img.src = gifData.data.images.original.url;
+    } catch (err) {
         console.log(err);
         img.src = './wrong-GIPHY.gif';
-    });  
+    }
 }
 
 const img = document.querySelector('img');
