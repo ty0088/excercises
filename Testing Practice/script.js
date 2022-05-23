d67301b02a6125c9eba58beb5b69745feba114af
ty0088 <95297807+ty0088@users.noreply.github.com>
2022-05-23 16:09:53 +0100

    Update to Testing Practice
    fixed export/import modules
    added first function/test for Capitalise

--- Testing Practice/script.js
+++ Testing Practice/script.js
@@ -1,4 +1,14 @@
-export {
-
+const capitilise = (string) => {
+    const stringArr = string.split('');
+    let firstChar = stringArr[0];
+    firstChar = firstChar.toUpperCase();
+    let newArr = stringArr.slice(1);
+    newArr.unshift(firstChar);
+    return newArr.join('');
 }
 
+capitilise('tom');
+
+export {
+    capitilise,
+}
--- Testing Practice/script.test.js
+++ Testing Practice/script.test.js
@@ -1,3 +1,11 @@
 import {
-
+    capitilise,
 } from './script'
+
+test('Capitalise example 1', () => {
+    expect(capitilise('tom')).toBe('Tom');
+});
+
+test('Capitalise example 1', () => {
+    expect(capitilise('d124')).toBe('D124');
+});
