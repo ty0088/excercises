72ffb48d5f56fc46c8468b512e997e1b916a5c5f
ty0088 <95297807+ty0088@users.noreply.github.com>
2022-05-24 15:30:08 +0100

    Completed Testing Practice

--- Testing Practice/script.js
+++ Testing Practice/script.js
@@ -1,5 +1,5 @@
 const capitilise = (string) => {
-    const stringArr = string.split('');
+    const stringArr = [...string];
     let firstChar = stringArr[0];
     firstChar = firstChar.toUpperCase();
     let newArr = stringArr.slice(1);
@@ -7,8 +7,107 @@ const capitilise = (string) => {
     return newArr.join('');
 }
 
-capitilise('tom');
+const reverseStr = (string) => {
+    let strArr = [...string];
+    strArr.reverse();
+    return strArr.join("");
+}
+
+const calculator = {
+    add: (a, b) => {
+        return a + b;
+    },
+    subtract: (a, b) => {
+        return a - b;
+    },
+    divide: (a, b) => {
+        return a / b;
+    },
+    multiply: (a, b) => {
+        return a * b;
+    },
+}
+
+const caesarCipher = (string, shift) => {
+
+    if (shift > 26 || shift < -26) {
+        throw new Error('Cannot shift by more than 26 or less than -26');
+    }
+
+    const codeShift = (code, shift) => { //function to shift a letter by determined amount
+        let shiftCode = 0;
+        if (code >= 65 && code <= 90) { //shift upper case letters only
+            shiftCode = code + shift;
+            if (shiftCode > 90) {
+                shiftCode = shiftCode - 26;
+            } else if (shiftCode < 65) {
+                shiftCode = shiftCode + 26;
+            }
+        } else if (code >= 97 && code <= 122) { //shift lower case letter only
+            shiftCode = code + shift;
+            if (shiftCode > 122) {
+                shiftCode = shiftCode - 26;
+            } else if (shiftCode < 97) {
+                shiftCode = shiftCode + 26;
+            }
+        } else {
+            shiftCode = code; //do not shift anything that isn't a letter
+        }
+        return shiftCode;
+    }
+
+    const strArr = [...string];
+    const shiftStr = strArr.map(char => char.charCodeAt())
+                        .map(code => codeShift(code, shift))
+                        .map(code => String.fromCharCode(code))
+                        .join('');
+    return shiftStr;
+}
+
+function isLetter(char) {
+    return char.match(/[A-Za-z]/);
+  }
+  
+  function isUpperCase(char) {
+    return char === char.toUpperCase();
+  }
+  
+  function caesarCipher2(str, num) {
+    const stringArray = [...str];
+    let newArray = [];
+  
+    for (let char of stringArray) {
+      if (isLetter(char)) {
+        const charCode = char.toLowerCase().charCodeAt(0);
+        const shiftedCode = (((charCode - 97 + num) % 26) + 97);
+        const newLetter = String.fromCharCode(shiftedCode);
+  
+        if (isUpperCase(char)) {
+          newArray.push(newLetter.toUpperCase());
+        } else {
+          newArray.push(newLetter);
+        }
+      } else {
+        newArray.push(char);
+      }
+    }
+    return newArray.join('');
+  }
+
+const analyseArray = (arr) => {
+    const length = arr.length;
+    const average = arr.reduce((prev, curr) => prev + curr, 0) / length;
+    const min = Math.min(...arr);
+    const max = Math.max(...arr);
+    return {average, min, max, length};
+}
+
 
 export {
     capitilise,
+    reverseStr,
+    calculator,
+    caesarCipher,
+    analyseArray,
+    caesarCipher2
 }
--- Testing Practice/script.test.js
+++ Testing Practice/script.test.js
@@ -1,11 +1,111 @@
 import {
     capitilise,
+    reverseStr,
+    calculator,
+    caesarCipher,
+    analyseArray,
+    caesarCipher2
 } from './script'
 
 test('Capitalise example 1', () => {
     expect(capitilise('tom')).toBe('Tom');
 });
 
-test('Capitalise example 1', () => {
+test('Capitalise example 2', () => {
     expect(capitilise('d124')).toBe('D124');
 });
+
+test('Reverse String example 1', () => {
+    expect(reverseStr('123')).toBe('321');
+});
+
+test('Reverse String example 2', () => {
+    expect(reverseStr('cba')).toBe('abc');
+});
+
+test('Reverse String example 3', () => {
+    expect(reverseStr('Hello there!')).toBe('!ereht olleH');
+});
+
+test('Calculator add example 1', () => {
+    expect(calculator.add(1,1)).toBe(2);
+});
+
+test('Calculator add example 2', () => {
+    expect(calculator.add(0,5)).toBe(5);
+});
+
+test('Calculator subtract example 1', () => {
+    expect(calculator.subtract(1,1)).toBe(0);
+});
+
+test('Calculator subtract example 2', () => {
+    expect(calculator.subtract(5,10)).toBe(-5);
+});
+
+test('Calculator divide example 1', () => {
+    expect(calculator.divide(10,5)).toBe(2);
+});
+
+test('Calculator divide example 2', () => {
+    expect(calculator.divide(0,2)).toBe(0);
+});
+
+test('Calculator multiply example 1', () => {
+    expect(calculator.multiply(1,1)).toBe(1);
+});
+
+test('Calculator multiply example 2', () => {
+    expect(calculator.multiply(5,-5)).toBe(-25);
+});
+
+test('Cipher example 1', () => {
+    expect(caesarCipher('abcdefghijklmnopqrstuvwxyz', 1)).toBe('bcdefghijklmnopqrstuvwxyza');
+});
+
+test('Cipher example 2', () => {
+    expect(caesarCipher('Hello, how are you?', 1)).toBe('Ifmmp, ipx bsf zpv?');
+});
+
+test('Cipher example 3', () => {
+    expect(caesarCipher('AbC ZXy 123 <>#:`', 5)).toBe('FgH ECd 123 <>#:`');
+});
+
+test('Cipher example 4', () => {
+    expect(caesarCipher('abcXYZ', -3)).toBe('xyzUVW');
+});
+
+test('Cipher example 5', () => {
+    expect(caesarCipher('aA', -26)).toBe('aA');
+});
+
+test('Wraps from z to a', () => {
+    expect(caesarCipher('Z', 2)).toEqual('B');
+  });
+  
+  test('Works with different cases', () => {
+    expect(caesarCipher('mooniiDev', 5)).toEqual('rttsnnIja');
+  });
+  
+  test('Works with punctuation', () => {
+    expect(caesarCipher('Hello, World!', 5)).toEqual('Mjqqt, Btwqi!');
+  });
+
+test('Analyse Array example 1', () => {
+    expect(analyseArray([1,8,3,4,2,6])).toEqual({
+        average: 4,
+        min: 1,
+        max: 8,
+        length: 6
+      })
+});
+
+test('Analyse Array example 1', () => {
+    expect(analyseArray([0,1,2,3])).toEqual({
+        average: 1.5,
+        min: 0,
+        max: 3,
+        length: 4
+      })
+});
+
